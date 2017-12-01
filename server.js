611026815325039faf1ad3fde1ac9fedd37ad9b8
Yesh Gowda <yeshwanth.gowda@target.com>
2017-11-30 19:23:25 -0800

    Added a new feature

--- server.js
+++ server.js
@@ -1,25 +1,26 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const SocketServer = require('ws').Server;
-const path = require('path');
+const express = require("express");
+const SocketServer = require("ws").Server;
+const path = require("path");
 
 const PORT = process.env.PORT || 3000;
-const INDEX = path.join(__dirname, 'index.html');
+const INDEX = path.join(__dirname, "index.html");
+var VIDEO_IDX = 0;
 
 const server = express()
-  .use((req, res) => res.sendFile(INDEX) )
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+  .use((req, res) => res.sendFile(INDEX))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const wss = new SocketServer({ server });
 
-wss.on('connection', (ws) => {
-  console.log('Client connected');
-  ws.on('close', () => console.log('Client disconnected'));
+wss.on("connection", ws => {
+  console.log("Client connected");
+  ws.on("close", () => console.log("Client disconnected"));
 });
 
 setInterval(() => {
-  wss.clients.forEach((client) => {
-    client.send(new Date().toTimeString());
+  wss.clients.forEach(client => {
+    client.send(${PORT} + new Date().toTimeString());
   });
 }, 1000);
